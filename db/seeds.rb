e35e2e3c631648df1cd8b212221cd34c117a731e
Jorge Guerra <jorguerra@gmail.com>
2019-03-29 20:57:46 -0300

    Tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create([
+    {
+      picture: 'http://arcdn.ar-cdn.com/recipes/port500/6fc6f7e1-36c2-4308-b114-8dcb246f712c.jpg',
+      title: 'Comer empanadas'
+    },
+    {
+      picture: 'http://www.chilenaup.cl/wp-content/uploads/2017/09/CUECA.jpg',
+      title: 'Bailar cueca'
+    },
+    {
+      picture: 'https://whereismyspoon.co/wp-content/uploads/2018/07/choripan-with-chimichurri-3.jpg',
+      title: 'Comerse un choripan'
+    },
+    {
+      picture: 'https://www.laylita.com/recetas/wp-content/uploads/Chicha-de-pi%C3%B1a-preparacion-7.jpg',
+      title: 'Tomar chicha'
+    },
+    {
+      picture: 'https://www.carolina.cl/static/2018/09/fonda-952x600.jpg',
+      title: 'Ir a fonda'
+    },
+    {
+      picture: 'https://www.aprenderjuntos.cl/wp-content/uploads/2016/09/emboque-portada.jpg',
+      title: 'Jugar al emboque'
+    },
+    {
+      picture: 'https://images.theconversation.com/files/250085/original/file-20181211-76962-1oqjng9.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=496&fit=clip',
+      title: 'Chupar como esponja'
+    },
+    {
+      picture: 'https://media.metrolatam.com/2019/02/07/befunkyc555ollage2-e90e8d820ac7395e4b12873e7978822f-1200x600.jpg',
+      title: 'Pelearse con un gueon'
+    },
+    {
+      picture: 'https://thenypost.files.wordpress.com/2015/12/hangover1a.jpg?quality=90&strip=all&w=618&h=410&crop=1',
+      title: 'Terminar con resaca'
+    }
+  ])
